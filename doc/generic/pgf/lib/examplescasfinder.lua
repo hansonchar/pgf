5d6c904f3a64c6575f763db92b198db6d7c50723
Hanson Char <hanson.char@gmail.com>
2024-06-19 21:57:08 -0700

    ditto - add unit tests

--- doc/generic/pgf/lib/examplescasfinder.lua
+++ doc/generic/pgf/lib/examplescasfinder.lua
@@ -71,22 +71,21 @@ finder.grammar =
   content = Cf(Ct "" * Cg(V("options") ^ -1 * Ct(V("exentry") ^ 1)), reorg),
   options = V "optionskv" * P(",") ^ -1,
   optionskv = Cg(C("options") * SP * "=" * SP * str),
-  exentry = str * P(",") ^ -1,
+  exentry = str * P(",") ^ -1
 }
 
--- local function preamble(options)
---   local p = SP * P "preamble" * SP * "=" * SP * C(P(1) ^ 1)
---   local matches = p:match(u.get_string(options))
---   local table = {}
---   if matches then
---     table.preamble = matches
---   end
---   return table
--- end
+local function preamble(options)
+  local p = SP * P "preamble" * SP * "=" * SP * C(P(1) ^ 1)
+  local matches = p:match(u.get_string(options))
+  local table = {}
+  if matches then
+    table.preamble = matches
+  end
+  return table
+end
 
-function finder.get_options(_)
-  -- return e.options and preamble(e.options) or {}
-  return {}
+function finder.get_options(e)
+  return e.options and preamble(e.options) or {}
 end
 
 function finder.get_content(e)
@@ -102,8 +101,7 @@ if not UNIT_TESTING then
 end
 
 -- Unit tests and debugging
-local test_case1 =
-  [=[
+local test_case1 = [=[
   examples = {
     [[ code example 1 ]],
     [[ code example 2 ]]
@@ -112,15 +110,33 @@ local test_case1 =
 
 do
   local matches = finder.grammar:match(test_case1)
-  print(tostring(matches))
-  -- assert(#matches == 2)
-  -- assert(u.strip(matches[1].code) == "first entry code")
-  -- assert(u.strip(matches[1].options) == "first entry options")
-  -- assert(u.strip(matches[2].code) == "second entry code")
-  -- assert(not matches[2].options)
+  assert(#matches == 2)
+  assert(u.strip(matches[1].code) == "code example 1")
+  assert(not matches[1].options)
+  assert(u.strip(matches[2].code) == "code example 2")
+  assert(not matches[2].options)
 end
 
-local test_case2 = [=[
+local test_case2 =
+  [=[
+  examples = {
+    options = "options for all",
+    [[ code example 1 ]],
+    [[ code example 2 ]]
+  }
+]=]
+
+do
+  local matches = finder.grammar:match(test_case2)
+  assert(#matches == 2)
+  assert(u.strip(matches[1].code) == "code example 1")
+  assert(u.strip(matches[1].options) == "options for all")
+  assert(u.strip(matches[2].code) == "code example 2")
+  assert(u.strip(matches[2].options) == "options for all")
+end
+
+local test_case3 =
+  [=[
   examples = {[["
     \tikz \graph [simple necklace layout, node distance=0cm, nodes={circle,draw}]
       { 1--2--3--4--5--1 };
@@ -131,4 +147,58 @@ local test_case2 = [=[
   "]]
   }
 ]=]
+
+do
+  local matches = finder.grammar:match(test_case3)
+  assert(#matches == 2)
+  assert(not matches[1].options)
+  assert(
+    u.strip(u.get_string(matches[1].code)) ==
+      [[\tikz \graph [simple necklace layout, node distance=0cm, nodes={circle,draw}]
+      { 1--2--3--4--5--1 };]]
+  )
+  assert(not matches[2].options)
+  assert(
+    u.strip(u.get_string(matches[2].code)) ==
+      [[\tikz \graph [simple necklace layout, node distance=0cm, node sep=0mm,
+                  nodes={circle,draw}]
+      { 1--2--3[node pre sep=5mm]--4--5[node pre sep=1mm]--1 };]]
+  )
+end
+
+local test_case4 =
+  [=[
+  examples = {
+    options = [[ preamble={\usetikzlibrary{graphs,graphdrawing} \usegdlibrary{force}} ]],
+    [[
+        \tikz \graph [spring electrical layout, horizontal=0 to 1]
+          { 0 [electric charge=1] -- subgraph C_n [n=10] };
+    ]],
+    [[
+        \tikz \graph [spring electrical layout, horizontal=0 to 1]
+          { [clique] 1 [electric charge=5], 2, 3, 4 };
+    ]]
+  }
+  ]=]
+
+do
+  local matches = finder.grammar:match(test_case4)
+  assert(#matches == 2)
+  assert(
+    u.strip(u.get_string(matches[1].options)) ==
+      [[preamble={\usetikzlibrary{graphs,graphdrawing} \usegdlibrary{force}}]]
+  )
+  assert(
+    u.strip(u.get_string(matches[1].code)) ==
+      [[\tikz \graph [spring electrical layout, horizontal=0 to 1]
+          { 0 [electric charge=1] -- subgraph C_n [n=10] };]]
+  )
+  assert(matches[2].options == matches[1].options)
+  assert(
+    u.strip(u.get_string(matches[2].code)) ==
+      [[\tikz \graph [spring electrical layout, horizontal=0 to 1]
+          { [clique] 1 [electric charge=5], 2, 3, 4 };]]
+  )
+end
+
 return finder
