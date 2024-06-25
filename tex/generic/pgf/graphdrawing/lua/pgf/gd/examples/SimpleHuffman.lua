05ac99d2ca835e25153cf7572d67dc00223d3e96
Hanson Char <hanson.char@gmail.com>
2024-06-25 15:01:12 -0700

    ditto

--- tex/generic/pgf/graphdrawing/lua/pgf/gd/examples/SimpleHuffman.lua
+++ tex/generic/pgf/graphdrawing/lua/pgf/gd/examples/SimpleHuffman.lua
@@ -53,8 +53,10 @@ declare {
 
 \begin{codeexample}[preamble={\usetikzlibrary{graphs,graphdrawing,quotes} \usegdlibrary{examples}}]
 \pgfgdset{
-  HuffmanLabel/.style={/tikz/edge node={node[fill=white,font=\footnotesize,inner sep=1pt]{#1}}},
-  HuffmanNode/.style={/tikz/.cd,circle,inner sep=0pt,outer sep=0pt,draw,minimum size=3pt}
+  HuffmanLabel/.style={
+    /tikz/edge node={node[fill=white,font=\footnotesize,inner sep=1pt]{#1}}},
+  HuffmanNode/.style={
+    /tikz/.cd,circle,inner sep=0pt,outer sep=0pt,draw,minimum size=3pt}
 }
 \tikz \graph [simple Huffman layout,
               level distance=7mm, sibling distance=8mm, grow'=up]
